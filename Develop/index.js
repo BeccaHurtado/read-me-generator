63896ba82df874f6d6851b42557fa1d9b272eb2c
BeccaHurtado <rebeccah7.rh@gmail.com>
2022-02-23 10:28:16 -0700

    first question

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,24 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require('inquirer');
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name');
+                    return false;
+                }
+            }
+        }
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
